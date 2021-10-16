68d45328b79b95d97b7658fc281aad3d718e90af
Fabian <13519140@std.stei.itb.ac.id>
2021-10-16 11:49:17 +0700

    edit local search score

--- src/ai/minimax.py
+++ src/ai/minimax.py
@@ -1,46 +1,70 @@
 import random
 from time import time
 
-from src.constant import ShapeConstant
-from src.model import State
+from src.constant import ShapeConstant, GameConstant
+from src.model import State, Board, Piece
+from src.utility import is_full, is_out, check_streak, getRow, score
 
 from typing import Tuple, List
+
 '''
-Langkah menentukan value/nilai
-1. cari row dari column dengan fungsi place pada utility
-2. win priority shape>color
-3. waktu berfikir diimplement disini, tapi caranya gimanaaaa
-4. local search pake simulated annealing, pake asumsi T=1000 mungkin
-
-
-Algoritma
-1. set max time
-2. random
-3. hitung nilai
-4. kalau lebih baik ambil
-5. lebih buruk coba random lalu compare dengan T
-6. random ok => ambil return
-7. 
-
-best = random
-while(self.thinking_time >= time()):
-    #body
-    #doing SA
-
-    #if ok then best = SA
-
-ret best;
+Minimax Alpha Beta Pruning
 '''
-
 class Minimax:
     def __init__(self):
-        pass
+        self.n_player = 0
+        self.board = None
+        self.thinking_time = 0
+        self.alpha = 1000
+        self.beta = -1000
     
-    def MinMax(self):
-        pass
+    def _score(self, solusi : Tuple[str, str]) -> int:
+        #daftar skor
+        # -1 ketika is out
+        #  0 ketika check streak None
+        # 1 ketika check streak mendapat color
+        # 2 ketika check streak mendapat shape
+        skor = -1
+        if self.board == None or self.state == None : return -1
+        curr_col = solusi[0]
+        curr_row = getRow(self.state, self.n_player, solusi[1], curr_col)
+
+        print(curr_row)
+        if is_out(self.board, curr_row, curr_col):
+            return -1
+        
+        cs = check_streak(self.board, curr_row, curr_col)
+        skor = 0 if cs == None else (2 if cs[0] == GameConstant.SHAPE else 1)
+        
+        return skor
+
+    def successors(self) -> List[Tuple[str, str]]:
+        """
+        Menghasilkan list suksesor dari sebuah state board
+        """
+        succList = []
+        
+        for i in range(7):
+            succList.append((i, ShapeConstant.CIRCLE))
+            succList.append((i, ShapeConstant.CROSS))
+
+        return succList
+
+
+    def minimax(self, state: State) -> Tuple[str, str]:
+        succList = self.successors()
+
+        for succ in succList:
+            print(succ, "Score: ", score(succ, state, self.n_player))
+
+        return (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE]))
 
     def find(self, state: State, n_player: int, thinking_time: float) -> Tuple[str, str]:
         self.thinking_time = time() + thinking_time
-        best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
-
+        self.n_player = n_player
+        self.state = state
+        self.board = state.board
+        
+        best_movement = self.minimax(state)
+            
         return best_movement
