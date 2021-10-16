adfc8a8ba81c4329542de62d14d9017d85dc9ca0
Fabian <13519140@std.stei.itb.ac.id>
2021-10-16 14:12:22 +0700

    create structure for minmax alpha beta pruning, TODO: objective function

--- src/ai/minimax.py
+++ src/ai/minimax.py
@@ -1,70 +1,114 @@
 import random
 from time import time
-
-from src.constant import ShapeConstant, GameConstant
-from src.model import State, Board, Piece
-from src.utility import is_full, is_out, check_streak, getRow, score
-
 from typing import Tuple, List
+from copy import deepcopy
+
+from src.constant import ShapeConstant
+from src.model import Board, State, Piece
+from src.utility import getRow, score, place
 
 '''
 Minimax Alpha Beta Pruning
 '''
 class Minimax:
     def __init__(self):
-        self.n_player = 0
-        self.board = None
         self.thinking_time = 0
-        self.alpha = 1000
-        self.beta = -1000
+        self.best_movement = (random.randint(0, 6), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE]))
     
-    def _score(self, solusi : Tuple[str, str]) -> int:
-        #daftar skor
-        # -1 ketika is out
-        #  0 ketika check streak None
-        # 1 ketika check streak mendapat color
-        # 2 ketika check streak mendapat shape
-        skor = -1
-        if self.board == None or self.state == None : return -1
-        curr_col = solusi[0]
-        curr_row = getRow(self.state, self.n_player, solusi[1], curr_col)
-
-        print(curr_row)
-        if is_out(self.board, curr_row, curr_col):
-            return -1
-        
-        cs = check_streak(self.board, curr_row, curr_col)
-        skor = 0 if cs == None else (2 if cs[0] == GameConstant.SHAPE else 1)
-        
-        return skor
-
-    def successors(self) -> List[Tuple[str, str]]:
+    def getMoves(self, n_player, state: State) -> List[Tuple[str, str]]:
         """
         Menghasilkan list suksesor dari sebuah state board
         """
-        succList = []
-        
+        moveList = []
+        player = state.players[n_player]
+
         for i in range(7):
-            succList.append((i, ShapeConstant.CIRCLE))
-            succList.append((i, ShapeConstant.CROSS))
+            if player.quota[ShapeConstant.CROSS] != 0:
+                moveList.append((i, ShapeConstant.CROSS))
 
-        return succList
+            if player.quota[ShapeConstant.CIRCLE] != 0:
+                moveList.append((i, ShapeConstant.CIRCLE))
 
+        return moveList
 
-    def minimax(self, state: State) -> Tuple[str, str]:
-        succList = self.successors()
+    def minimax(self, n_player: int, state: State, depth: int) -> Tuple[str, str]:
+        # Inisialisasi alpha beta
+        alpha = -1000
+        nodeScore = -1000
+        beta = 1000
+        moveList = self.getMoves(n_player, state)
+        
+        for move in moveList:
+            # Deep copy state and do move
+            newState = deepcopy(state)
+            valid = place(newState, n_player, move[1], move[0])
 
-        for succ in succList:
-            print(succ, "Score: ", score(succ, state, self.n_player))
+            if (valid != -1):
+                # Get opponent action
+                minValue = self.minABValue(newState, (n_player+1)%2, alpha, beta, depth+1)
+                
+                if minValue > alpha:
+                    nodeScore = minValue
+                    self.best_movement = move
 
-        return (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE]))
+                alpha = max(nodeScore, alpha)
+
+        return self.best_movement
+
+    def minABValue(self, state: State, n_player: int, alpha: int, beta: int, depth: int) -> int:
+        """
+        Cari skor minimum dengan melakukan aksi musuh. Mengembalikan skor.
+        """
+        nodeScore = 1000
+        moveList = self.getMoves(n_player, state)
+
+        for move in moveList:
+            # Deep copy state and do move
+            newState = deepcopy(state)
+            valid = place(newState, n_player, move[1], move[0])
+            # If placement valid, count nodeScore
+            if (valid != -1):
+                print(newState.board)
+                nodeScore = min(nodeScore, self.maxABValue(newState, (n_player+1)%2, alpha, beta, depth+1))
+                # Prune
+                if (nodeScore <= alpha):
+                    return nodeScore
+
+                beta = min(beta, nodeScore)
+
+        return nodeScore
+
+    def maxABValue(self, state: State, n_player: int, alpha: int, beta: int, depth: int) -> int:
+        """
+        Cari skor maksimum.
+        """
+        # Apabila node daun, kembalikan score node berdasarkan objective function
+        if depth == 2:
+            return random.choice([-1, 0, 1, 2])
+
+        nodeScore = -1000
+        moveList = self.getMoves(n_player, state)
+
+        for move in moveList:
+            # Deep copy state and do move
+            newState = deepcopy(state)
+            valid = place(newState, n_player, move[1], move[0])
+
+            if (valid != -1):
+                nodeScore = max(nodeScore, self.minABValue(newState, (n_player+1)%2), alpha, beta, depth+1)
+                #if beta < nodeScore, prune
+                if (nodeScore >= beta):
+                    return nodeScore
+                #update alpha
+                alpha = max(alpha, nodeScore)
+
+        return nodeScore
 
     def find(self, state: State, n_player: int, thinking_time: float) -> Tuple[str, str]:
         self.thinking_time = time() + thinking_time
-        self.n_player = n_player
-        self.state = state
-        self.board = state.board
-        
-        best_movement = self.minimax(state)
+
+        while (time() < self.thinking_time):
+            self.minimax(n_player, state, 0)
+            break
             
-        return best_movement
+        return self.best_movement
