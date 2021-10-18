b7f0db270fd51bf126c6bdce52d9c9797837e09c
Fabian <13519140@std.stei.itb.ac.id>
2021-10-18 10:13:02 +0700

    change dumper

--- dumper.py
+++ dumper.py
@@ -1,15 +1,16 @@
-from src.ai import Minimax
+from src.ai import Minimax, LocalSearch
 from src.constant import Path
 from src.utility import dump
 
 def dumper(path_b1, path_b2, path_pvb):
-    model = Minimax()
-    dump(model, Path.BVB_P1.format(path_b1))
-    dump(model, Path.BVB_P2.format(path_b2))
-    dump(model, Path.PVB.format(path_pvb))
+    model1 = Minimax()
+    model2 = LocalSearch()
+    dump(model1, Path.BVB_P1.format(path_b1))
+    dump(model2, Path.BVB_P2.format(path_b2))
+    dump(model1, Path.PVB.format(path_pvb))
 
 if __name__ == '__main__':
-    bot1_filename = 'group1-minimax-1.pkl' 
-    bot2_filename = 'group1-minimax-2.pkl'
-    pvp_bot_filename = 'group1-minimax-1.pkl'
+    bot1_filename = 'PT_MENCARI_CINTA_SEJATI_minimax.pkl' 
+    bot2_filename = 'PT_MENCARI_CINTA_SEJATI_local_search.pkl'
+    pvp_bot_filename = 'PT_MENCARI_CINTA_SEJATI_minimax_pvp.pkl'
     dumper(bot1_filename, bot2_filename, pvp_bot_filename)
--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-python main.py --row 6 --col 7 --type bvb --player_choice 0 --thinking_time 3 --bot1 group1-minimax-1.pkl --bot2 group1-minimax-2.pkl
+python main.py --row 6 --col 7 --type bvb --player_choice 0 --thinking_time 3 --bot1 PT_MENCARI_CINTA_SEJATI_minimax.pkl --bot2 PT_MENCARI_CINTA_SEJATI_local_search.pkl
