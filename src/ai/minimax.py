00f31ba1c600e2f84d0948927836a8a038194efc
Fabian <13519140@std.stei.itb.ac.id>
2021-10-20 10:43:58 +0700

    change objective function for minimax

--- src/ai/minimax.py
+++ src/ai/minimax.py
@@ -10,56 +10,90 @@ from src.utility import place, is_out
 '''
 Minimax Alpha Beta Pruning
 '''
+def maxScore(tuple1: Tuple[str, int], tuple2: Tuple[str, int]) -> Tuple[str, int]:
+    if scoreGreater(tuple1, tuple2):
+        return tuple1
+    
+    if scoreGreater(tuple2, tuple1):
+        return tuple2
 
-def count_streak(board: Board, row: int, col: int) -> int:
+    return tuple1
+
+def minScore(tuple1: Tuple[str, int], tuple2: Tuple[str, int]) -> Tuple[str, int]:
+    if scoreSmaller(tuple1, tuple2):
+        return tuple1
+    
+    if scoreSmaller(tuple2, tuple1):
+        return tuple2
+
+    return tuple1
+
+def scoreGreater(tuple1: Tuple[str, int], tuple2: Tuple[str, int]) -> bool:
+    if tuple1[1] > tuple2[1]:
+        return True
+    
+    if tuple1[1] == tuple2[1] and tuple1[0] == GameConstant.WIN_PRIOR[0]:
+        return True
+
+    return False
+
+def scoreSmaller(tuple1: Tuple[str, int], tuple2: Tuple[str, int]) -> bool:
+    if tuple1[1] < tuple2[1]:
+        return True
+    
+    if tuple1[1] == tuple2[1] and tuple1[0] == GameConstant.WIN_PRIOR[1]:
+        return True
+
+    return False
+
+def count_streak(board: Board, row: int, col: int, prior: str) -> Tuple[str, int]:
     piece = board[row, col]
     if piece.shape == ShapeConstant.BLANK:
         return None
 
     # Arah mata angin
     streak_way = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-    ret_count = 1
-    # Loop prioritas kemenangan yaitu shape dulu baru color
-    for prior in GameConstant.WIN_PRIOR:
+    ret_count = (prior, 1)
+
+    # Pergerakan arah mata angin
+    for row_ax, col_ax in streak_way:
         mark = 1
-        # Pergerakan arah mata angin
-        for row_ax, col_ax in streak_way:
-            row_ = row + row_ax
-            col_ = col + col_ax
-            # Gerakan sebanyak 4 kali
-            for _ in range(GameConstant.N_COMPONENT_STREAK - 1):
-                # Apabila posisi di luar board, break
-                if is_out(board, row_, col_):
-                    break
+        row_ = row + row_ax
+        col_ = col + col_ax
+        # Gerakan sebanyak 4 kali
+        for _ in range(GameConstant.N_COMPONENT_STREAK - 1):
+            # Apabila posisi di luar board, break
+            if is_out(board, row_, col_):
+                break
 
-                # Apabila saat pengecekan streak shape dan piece bukan shape yang sama
-                shape_condition = (
-                    prior == GameConstant.SHAPE
-                    and piece.shape != board[row_, col_].shape
-                )
-                # Apabila saat pengecekan streak color dan piece pada board bukan color yang sama
-                color_condition = (
-                    prior == GameConstant.COLOR
-                    and piece.color != board[row_, col_].color
-                )
-                # Apabila terjadi salah satu dari kedua kondisi di atas, break
-                if shape_condition or color_condition:
-                    break
+            # Apabila saat pengecekan streak shape dan piece bukan shape yang sama
+            shape_condition = (
+                prior == GameConstant.SHAPE
+                and piece.shape != board[row_, col_].shape
+            )
+            # Apabila saat pengecekan streak color dan piece pada board bukan color yang sama
+            color_condition = (
+                prior == GameConstant.COLOR
+                and piece.color != board[row_, col_].color
+            )
+            # Apabila terjadi salah satu dari kedua kondisi di atas, break
+            if shape_condition or color_condition:
+                break
 
-                row_ += row_ax
-                col_ += col_ax
-                mark += 1
+            row_ += row_ax
+            col_ += col_ax
+            mark += 1
 
-            if mark > ret_count:
-                ret_count = mark
+        if mark > ret_count[1]:
+            ret_count = (prior, mark)
 
-            # Apabila ada streak berjumlah 4, langsung return
-            if mark == GameConstant.N_COMPONENT_STREAK:
-                return mark
+        # Apabila ada streak berjumlah 4, langsung return
+        if mark == GameConstant.N_COMPONENT_STREAK:
+            return (prior, mark)
     
     return ret_count
 
-def score(state: State, n_player: int) -> int:
+def score(state: State, n_player: int) -> Tuple[str, int]:
     """
     [DESC]
         Fungsi objective function
@@ -71,14 +105,20 @@ def score(state: State, n_player: int) -> int:
         Range dari 0 sampai 4.
     """
     board = state.board
-    stateScore = 0
+    stateScore = ("NONE", 0)
     # Penelusuran seluruh kolom dan baris
     for row in range(board.row):
         for col in range(board.col):
+            # Apabila bentuk bidak sama dengan bentuk pemain, mulai penghitungan streak
+            if board[row,col].shape == state.players[n_player].shape:
+                tempScore = count_streak(board, row, col, GameConstant.WIN_PRIOR[0])
+                if scoreGreater(tempScore, stateScore):
+                    stateScore = tempScore
+
             # Apabila warna bidak sama dengan warna pemain, mulai penghitungan streak
             if board[row,col].color == state.players[n_player].color:
-                tempScore = count_streak(board, row, col)
-                if tempScore > stateScore:
+                tempScore = count_streak(board, row, col, GameConstant.WIN_PRIOR[1])
+                if scoreGreater(tempScore, stateScore):
                     stateScore = tempScore
                 
     return stateScore
@@ -106,9 +146,9 @@ class Minimax:
 
     def minimax(self, n_player: int, state: State, depth: int) -> Tuple[str, str]:
         # Inisialisasi alpha beta
-        alpha = -1000
-        nodeScore = -1000
-        beta = 1000
+        alpha = (GameConstant.WIN_PRIOR[1], -1000)
+        nodeScore = (GameConstant.WIN_PRIOR[1], -1000)
+        beta = (GameConstant.WIN_PRIOR[0], 1000)
         # Get move list
         moveList = self.getMoves(n_player, state)
         
@@ -120,20 +160,20 @@ class Minimax:
             if (valid != -1):
                 # Get opponent action
                 minValue = self.minABValue(newState, (n_player+1)%2, alpha, beta, depth+1)
-                
-                if minValue > alpha:
+
+                if scoreGreater(minValue, alpha):
                     nodeScore = minValue
                     self.best_movement = move
 
-                alpha = max(nodeScore, alpha)
+                alpha = maxScore(nodeScore, alpha)
 
         return self.best_movement
 
-    def minABValue(self, state: State, n_player: int, alpha: int, beta: int, depth: int) -> int:
+    def minABValue(self, state: State, n_player: int, alpha: Tuple[str, int], beta: Tuple[str, int], depth: int) -> Tuple[str, int]:
         """
         Cari skor minimum dengan melakukan aksi musuh. Mengembalikan skor.
         """
-        nodeScore = 1000
+        nodeScore = (GameConstant.WIN_PRIOR[0], 1000)
         moveList = self.getMoves(n_player, state)
 
         for move in moveList:
@@ -142,26 +182,25 @@ class Minimax:
             valid = place(newState, n_player, move[1], move[0])
             # If placement valid, count nodeScore
             if (valid != -1):
-                #print(newState.board)
-                nodeScore = min(nodeScore, self.maxABValue(newState, (n_player+1)%2, alpha, beta, depth+1))
+                nodeScore = minScore(nodeScore, self.maxABValue(newState, (n_player+1)%2, alpha, beta, depth+1))
                 # Prune
-                if (nodeScore <= alpha):
+                if scoreGreater(alpha, nodeScore):
                     return nodeScore
 
-                beta = min(beta, nodeScore)
+                beta = minScore(beta, nodeScore)
 
         return nodeScore
 
-    def maxABValue(self, state: State, n_player: int, alpha: int, beta: int, depth: int) -> int:
+    def maxABValue(self, state: State, n_player: int, alpha: Tuple[str, int], beta: Tuple[str, int], depth: int) -> Tuple[str, int]:
         """
         Cari skor maksimum. Mengembalikan score
         """
         # Apabila node daun, kembalikan score node berdasarkan objective function
-        if depth == 4:
+        if depth == 2:
             terminalScore = score(state, n_player)
             return terminalScore
 
-        nodeScore = -1000
+        nodeScore = (GameConstant.WIN_PRIOR[1], -1000)
         moveList = self.getMoves(n_player, state)
 
         for move in moveList:
@@ -170,12 +209,12 @@ class Minimax:
             valid = place(newState, n_player, move[1], move[0])
 
             if (valid != -1):
-                nodeScore = max(nodeScore, self.minABValue(newState, (n_player+1)%2, alpha, beta, depth+1))
+                nodeScore = maxScore(nodeScore, self.minABValue(newState, (n_player+1)%2, alpha, beta, depth+1))
                 #if beta < nodeScore, prune
-                if (nodeScore >= beta):
+                if scoreSmaller(beta, nodeScore):
                     return nodeScore
                 #update alpha
-                alpha = max(alpha, nodeScore)
+                alpha = maxScore(alpha, nodeScore)
 
         return nodeScore
 
@@ -185,5 +224,6 @@ class Minimax:
         while (time() < self.thinking_time):
             self.minimax(n_player, state, 0)
             break #Break if finished exploring successor tree
+        # print(score(state, n_player))
             
         return self.best_movement
